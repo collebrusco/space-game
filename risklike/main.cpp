17a06ac08d85011ed70608c7454b57823135bd7e
collebrusco <collebrusco.frank@gmail.com>
2023-06-02 15:54:51 -0500

    Initial Commit

---
+++ risklike/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  risklike
+//
+//  Created by Frank Collebrusco on 6/2/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
